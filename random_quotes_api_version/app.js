3647a449cf51681180c8b1115ae939025e54f551
mussol <gemma.valls@gmail.com>
2017-06-21 00:28:44 +0200

    new version API nodejs

---
+++ random_quotes_api_version/app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    request("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en", function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            //console.log(data);
+            res.render("home", {data: data});
+        }
+    });
+});
+
+app.listen(process.env.PORT, process.env.ID, function(req, res) {
+    console.log("Server started!");
+});
